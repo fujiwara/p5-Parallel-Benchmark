9bfc9d672f7fdf11e96c74ed46db756b0a0a04e2
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2012-02-17 16:47:20 +0900

    initial commit

---
+++ lib/Parallel/Benchmark.pm
@@ -0,0 +1,32 @@
+package Parallel::Benchmark;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Parallel::Benchmark -
+
+=head1 SYNOPSIS
+
+  use Parallel::Benchmark;
+
+=head1 DESCRIPTION
+
+Parallel::Benchmark is
+
+=head1 AUTHOR
+
+FUJIWARA Shunichiro E<lt>fujiwara@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
